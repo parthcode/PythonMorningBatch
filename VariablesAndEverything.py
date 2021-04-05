c37895b642644cf426ea1c6c1d4b347c0b26ecc5
parthamohanty <parthsit@gmail.com>
2021-04-05 08:25:49 +0530

    Variables

---
+++ VariablesAndEverything.py
@@ -0,0 +1,17 @@
+"""
+Varialbes:
+1.Basic
+2.Datastructure
+"""
+# Basic type of varialbes :
+number = 90
+sting = "Mumbai"
+float_value = 12.32
+# print(float_value)
+# print(type(float_value))
+# print(type(number))
+# print(type(sting))
+"""
+Ask user to enter Name , age and Mark.
+Print the above details
+"""
