f4e4ce0d9124997eecdeb5ea9dabfe8a596b4675
Gautham <gauthamk508@gmail.com>
2021-04-06 08:41:04 +0530

    firstcls

--- VariablesAndEverything.py
+++ VariablesAndEverything.py
@@ -15,10 +15,14 @@ float_value = 12.32
 Ask user to enter Name , age and Mark.
 Print the above details
 """
-
+name=input("enter the name")
+age=input("enter the age")
+marks=input("enter the marks")
+print("your details ar:",name,age,marks,sep="\n")
 """
 List :
 It is a type of array in Python which is a combination of different types of data
 """
 list_variable = [1, 2, 3, 45, 'London', 'Alex', 34.23]
 print(list_variable[5])
+
