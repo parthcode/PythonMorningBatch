af841ea0392e937ff53ea1220b2cdbdc46398a8b
parthamohanty <parthsit@gmail.com>
2021-04-08 08:35:25 +0530

    dictionaly and tuple

--- VariablesAndEverything.py
+++ VariablesAndEverything.py
@@ -15,9 +15,9 @@ float_value = 12.32
 Ask user to enter Name , age and Mark.
 Print the above details
 """
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-mark = input("Enter your mark: ")
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# mark = input("Enter your mark: ")
 # print("Your Name is :", name)
 # print("Your age is :", age)
 # print("Your Mark is :", mark)
@@ -25,9 +25,30 @@ mark = input("Enter your mark: ")
 List :
 It is a type of array in Python which is a combination of different types of data
 """
-list_variable = [name, age, mark]
+# list_variable = [name, age, mark]
 # list_variable.append(name)
 # list_variable.append(age)
 # list_variable.append(mark)
-list_variable.append("This is a new value")
-print(list_variable)
+# list_variable.append("This is a new value")
+# print(list_variable)
+"""
+Tuple:
+This is a type of data structure which is similar to list but the values in a tuple 
+are constant .
+"""
+# tuple_variable = ('alex', 23, 902.21)
+# print(tuple_variable[2])
+"""
+Dictionary:
+This is a type of data structure where the values are referenced with a key (#Hash)
+"""
+# dic_variable = {}
+# dict_variable = {'Name': 'Alex', 'Address': 'Mumbai', 'Mark': 90}
+# var = {key:value,key:value,key:value}
+# dict_variable['subject'] = 'Math'
+# print(dict_variable.keys())
+# print(dict_variable.values())
+"""
+Write a script to ask user name , year of birth and address 
+take the above user input and put in it in a dict {name:value,age:value,address:value}
+"""
