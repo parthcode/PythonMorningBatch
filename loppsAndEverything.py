89a324a02cf9d9a684c86ff2deab237cebc1e9f7
parthamohanty <parthsit@gmail.com>
2021-04-13 08:45:20 +0530

    series

--- loppsAndEverything.py
+++ loppsAndEverything.py
@@ -12,10 +12,34 @@ fibonacci series
 1,2,3,5,8.....
 ask user the range of the series and print the above series
 """
-start = 0
-end = 1
-for i in range(8):
-    result = start + end
-    print(result)
-    start = end
-    end = result
+# start = 0
+# end = 1
+# for i in range(8):
+#     result = start + end
+#     print(result)
+#     start = end
+#     end = result
+"""
+Filter the string and integer from a list to a new list
+"""
+# list_values = [1, 2, 3, 4, 'pwd', 43, 'abc', 'xyz']
+# list_int = []
+# list_string = []
+# for i in list_values:
+#     if type(i) == type(12):
+#         list_int.append(i)
+#     else:
+#         list_string.append(i)
+# print(list_int)
+# print(list_string)
+list_values = [1, 2, 3, 3, 'pwd', 2, 'abc', 'pwd']
+# find all the values which are repeated in the above list
+
+
+list_key = [2, 3, 4, 5, 6]
+dic_values = {}
+# Fill the above dict with the keys from the list and relevant square as values
+# example : {2:4, 3:9, 4:16...}
+
+str_value = 'AbcaBc'
+# convert all the cap letter to small letter
